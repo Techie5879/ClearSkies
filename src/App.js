3862c277d4451b7dec1eef771d2fbf22d68dd8ac
Techie5879 <aritrahtml@gmail.com>
2023-01-07 00:57:19 +0530

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/NavBar/NavBar.jsx";
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -7,15 +7,15 @@ import Predictor from "./pages/predictor";
 
 function App() {
   return (
-    <Router>
+    <div className='App'>
       <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-
-        <Route path="/predictor" element={<Predictor/>} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/predictor" element={<Predictor />}/>
+        </Routes>
+      
+    </div>
   );
 }
 export default App;
